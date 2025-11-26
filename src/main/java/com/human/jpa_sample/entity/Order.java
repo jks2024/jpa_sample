7c0b5833aaff081a9484988f9734e241d8296d56
jks2024 <jks2024@gmail.com>
2025-11-26 16:16:45 +0900

    11월26일 작업분 오후...

--- src/main/java/com/human/jpa_sample/entity/Order.java
+++ src/main/java/com/human/jpa_sample/entity/Order.java
@@ -33,7 +33,12 @@ public class Order {
     private LocalDateTime updateTime; // 수정일
 
     // 여기가 주인이 아니므로 mappedBy 속성으로 연관관계의 주인을 설정
-    @OneToMany (mappedBy = "order", cascade = CascadeType.ALL)  // 부모 엔티티의 모든 상태를 자식에서 전달 함, 부모가 지워 질 때 자식을 함께 삭제하는 경우
+    // 부모 엔티티의 모든 상태를 자식에서 전달 함, 부모가 지워 질 때 자식을 함께 삭제하는 경우
+    // orphanRemoval = true : 고아 객체 제거, 부모와 자식 관계가 끊어지는 경우 DB를 자동 삭제하는 기능, 주로 한군데에서만 참조되는 엔티티에 사용
+    @OneToMany (mappedBy = "order",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true,
+            fetch = FetchType.LAZY  // 실제 데이터가 필요한 순간까지 DB 조회를 미루는 것, @OneToMany 기본값이 LAZY
+    )
     private List<OrderItem> orderItems = new ArrayList<>();
-
 }
--- src/test/java/com/human/jpa_sample/repository/OrderRepositoryTest.java
+++ src/test/java/com/human/jpa_sample/repository/OrderRepositoryTest.java
@@ -78,6 +78,61 @@ class OrderRepositoryTest {
         log.info("주문 상품 개수 : {}", savedOrder.getOrderItems().size());
         log.info("첫번째 상품명 : {}", savedOrder.getOrderItems().get(0).getItem());;
 
+        // 고아 객체 제거하기 : 위의 코드를 재 사용해서 조회를 해보고 이 후에 List에 대해 remove(인덱스)를 했을 때 delete쿼리가 날아가는 지 확인
+        log.info("=============== 고아 객체 제거 로직 시작 ===============");
+        savedOrder.getOrderItems().remove(0);
+        em.flush();
+        em.clear();
+        Order finalOrder = orderRepository.findById(order.getId())
+                        .orElseThrow(EntityNotFoundException::new);
+        log.info("고아 객체 제거 후 상품 개수 확인 : {}", finalOrder.getOrderItems().size());
+    }
+
+    @Test
+    @DisplayName("지연 로딩 쿼리 시점 확인 테스트")
+    public void lazyLoadingTest() {
+        Order order = new Order();
+
+        for (int i = 0; i < 2; i++) {
+            Item item = new Item();
+            item.setItemNm("지연로딩 상품 " + i);
+            item.setPrice(10000);
+            item.setStockNumber(100);
+            item.setItemDetail("상세 설명 " + i);
+            itemRepository.save(item);
+
+            // 주문 목록 생성
+            OrderItem orderItem = new OrderItem();
+            orderItem.setItem(item);
+            orderItem.setCount(10);
+            orderItem.setOrderPrice(10000 + i);
+            orderItem.setOrder(order);
+            order.getOrderItems().add(orderItem);
+        }
+        // 주문 저장을 위해 회원 정보 생성
+        Member member = new Member();
+        member.setName("안유진");
+        member.setEmail("ayj@gmail.com");
+        member.setPwd("sphb8250");
+        member.setImage("http://google.co.kr");
+        memberRepository.save(member);
+
+        order.setMember(member);
+        order.setOrderStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        orderRepository.save(order);
+
+        em.flush();  // DB에 강제로 쓰기(insert) (원래는 에티티매니저가 적절한 시점에 쓰기 작업 진행)
+        em.clear();
+
+        log.info("================ Order 조회 시작 ==================");
+        // 조회 쿼리가 한번만 나감
+        Order savedOrder = orderRepository.findById(order.getId())
+                .orElseThrow(EntityNotFoundException::new);
+        log.info("주문자 이메일 조회 : {}", savedOrder.getMember().getEmail());  // 주문 아이템에 대한 쿼리 안나감
+
+        log.info("================= OrderItem 실제 사용 ===============");  // 이때 조회 쿼리가 나감
+        log.info("아이템의 개수 : {}", savedOrder.getOrderItems().size());
     }
 
 }
