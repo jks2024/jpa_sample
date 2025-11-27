803851322dce9ad391f37983b57ecc4d8e0cafdd
jks2024 <jks2024@gmail.com>
2025-11-27 10:26:05 +0900

    11월27일 오전

---
+++ src/main/java/com/human/jpa_sample/dto/SignUpReqDto.java
@@ -0,0 +1,14 @@
+package com.human.jpa_sample.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor // 매개변수가 없는 기본 생성자 생성, 내부적으로 JSON 역직렬화 시 사용 됨
+public class SignUpReqDto {
+    private String email;
+    private String pwd;
+    private String name;
+}
--- src/main/java/com/human/jpa_sample/repository/MemberRepository.java
+++ src/main/java/com/human/jpa_sample/repository/MemberRepository.java
@@ -4,6 +4,13 @@ import com.human.jpa_sample.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    // null 방지를 위해 사용하는 Wrapper Calss 임
+    Optional<Member> findByEmail(String email);  // 이메일을 전달 받아 회원 정보를 반환
+    boolean existsByEmail(String email);  // 이메일을 전달 받아 가입 여부 확인
+    Optional<Member> findByEmailAndPwd(String email, String pwd); // 로그인 성공/실패
+
 }
